edf8cb6438a8eda8c729ae0fa23b0e617ac21867
Gregory Desrosiers <gregpdesrosiers@gmail.com>
2017-08-05 20:17:49 -0700

    First commit for Android app

---
+++ Android App/app/src/main/java/io/github/gregpdessch/detergo/AssessmentActivity.java
@@ -0,0 +1,13 @@
+package io.github.gregpdessch.detergo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssessmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessment);
+    }
+}
